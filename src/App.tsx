d8a421eb52135764d1330008be739fa6c70bbd77
Tomasz Pietrzak <tomasz.pietrzak@a4bee.com>
2024-08-08 08:18:06 +0200

    add state configuration and TransactionChart usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 import { useFetchApi } from './hooks/useFetchApi'
+import type { EChartsOption } from "echarts";
+import { TransactionChart } from './components/TransactionChart';
 
 function App() {
-  const {data, error, isFetching, isLoading} = useFetchApi('BTCUSDT');
+  // empty options were provided in order to prevent chart form runtime errors
+  const [options, setOptions] = useState<EChartsOption>({
+    title: {},
+    tooltip: {},
+    xAxis: {},
+    yAxis: {},
+    series: []
+  });
+
+  const [refetchInterval, setRefetchInterval] = useState<number>();
+  const [symbol, setSymbol] = useState<string>('BTCUSDT');
+
+  const {data, error, isFetching, isLoading} = useFetchApi(symbol, refetchInterval);
 
   if (error) {return <div>Api error</div>}
   if (isFetching) {return <div>Updating data</div>}
@@ -14,7 +29,7 @@ function App() {
 
   return (
     <div style={{height: "900px", width: "900px"}}>
-      Data were pulled successfuly
+      <TransactionChart option={options}/>
     </div>
   )
 }
