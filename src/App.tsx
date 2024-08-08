1b04044fc70ec2a25e4b78e54d53ebf95196cd4c
Tomasz Pietrzak <tomasz.pietrzak@a4bee.com>
2024-08-08 08:00:52 +0200

    basic api integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { useFetchApi } from './hooks/useFetchApi'
 
 function App() {
+  const {data, error, isFetching, isLoading} = useFetchApi('BTCUSDT');
+
+  if (error) {return <div>Api error</div>}
+  if (isFetching) {return <div>Updating data</div>}
+  if (isLoading) {return <div>Loading data</div>}
+
+  if(data) {
+    console.log(data)
+  }
 
   return (
-    <>
-      
-    </>
+    <div>
+      Data were pulled successfuly
+    </div>
   )
 }
 
---
+++ src/hooks/useFetchApi.tsx
@@ -0,0 +1,34 @@
+// this hook is created in order to exclude logic from component
+// tanstack-query was used in order to handle errors and refetching strategy. additionaly I never used tanstack-query in project that will have CR
+
+import { useQuery } from "@tanstack/react-query";
+
+export type apiResponseElement = {
+    M: boolean,
+    T: number
+    a: number
+    f: number
+    l: number
+    m: boolean
+    p: string
+    q: string
+}
+
+export const useFetchApi = (symbol: string, refetchInterval?: number) => {
+    const url = `https://api.binance.com/api/v3/aggTrades?symbol=${symbol}`
+    const {data, error, isFetching, isLoading} = useQuery<Array<apiResponseElement>>({
+        queryKey: ['fetchBinanceData'],
+        queryFn: async () => {
+            // in this case axios would be overkill - we need only one fetch here
+            const response = await fetch(url);
+            return response.json();
+        },
+        refetchInterval: refetchInterval
+    })
+    return {
+        data,
+        error,
+        isFetching,
+        isLoading
+    }
+}
